6594a0ac199a77018557709e9091f56c1395acdb
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-07-02 23:30:00 +0200

    build(backbone): Publish the Backbone to the GitLab Registry

--- backbone/build.gradle.kts
+++ backbone/build.gradle.kts
@@ -2,6 +2,7 @@
 
 plugins {
 	kotlin("multiplatform")
+	id("maven-publish")
 }
 
 kotlin {
@@ -28,5 +29,29 @@ kotlin {
 				api(KotlinX.coroutines.test)
 			}
 		}
+	}
+}
+
+publishing {
+	repositories {
+		val projectId = System.getenv("CI_PROJECT_ID")
+		val token = System.getenv("CI_JOB_TOKEN")
+
+		if (projectId != null && token != null)
+			maven {
+				name = "GitLab"
+				url = uri("https://gitlab.com/api/v4/projects/$projectId/packages/maven")
+
+				credentials(HttpHeaderCredentials::class.java) {
+					name = "Job-Token"
+					value = token
+				}
+
+				authentication {
+					create<HttpHeaderAuthentication>("header")
+				}
+			}
+		else
+			println("The GitLab registry is disabled because credentials are missing.")
 	}
 }
