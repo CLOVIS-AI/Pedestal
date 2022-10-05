31668c3e2f2581096a8ae4357694b8a105e19a5e
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-10-05 20:53:34 +0200

    feat(logger): Allow logging null values

--- logger/src/jsMain/kotlin/opensavvy.logger/Logger.kt
+++ logger/src/jsMain/kotlin/opensavvy.logger/Logger.kt
@@ -3,23 +3,23 @@ package opensavvy.logger
 private class ConsoleLogger(private val self: Any) : Logger {
 	override var level = LogLevel.default
 
-	override fun forceTrace(message: String, vararg objects: Any) {
+	override fun forceTrace(message: String, vararg objects: Any?) {
 		console.log(self::class.simpleName, message, *objects)
 	}
 
-	override fun forceDebug(message: String, vararg objects: Any) {
+	override fun forceDebug(message: String, vararg objects: Any?) {
 		console.log(self::class.simpleName, message, *objects)
 	}
 
-	override fun forceInfo(message: String, vararg objects: Any) {
+	override fun forceInfo(message: String, vararg objects: Any?) {
 		console.info(self::class.simpleName, message, *objects)
 	}
 
-	override fun forceWarn(message: String, vararg objects: Any) {
+	override fun forceWarn(message: String, vararg objects: Any?) {
 		console.warn(self::class.simpleName, message, *objects)
 	}
 
-	override fun forceError(message: String, vararg objects: Any) {
+	override fun forceError(message: String, vararg objects: Any?) {
 		console.error(self::class.simpleName, message, *objects)
 	}
 }
--- logger/src/jvmMain/kotlin/opensavvy/logger/Logger.kt
+++ logger/src/jvmMain/kotlin/opensavvy/logger/Logger.kt
@@ -7,7 +7,7 @@ private class SlfLogger(self: Any): Logger {
 	private val logger = LoggerFactory.getLogger(self::class.java)
 		?: error("Could not find a logger for ${self::class.java}")
 
-	private fun generateFormat(message: String, vararg objects: Any): String {
+	private fun generateFormat(message: String, vararg objects: Any?): String {
 		var result = message
 
 		repeat(objects.size) {
@@ -17,23 +17,23 @@ private class SlfLogger(self: Any): Logger {
 		return result
 	}
 
-	override fun forceTrace(message: String, vararg objects: Any) {
+	override fun forceTrace(message: String, vararg objects: Any?) {
 		logger.trace(generateFormat(message, *objects), *objects)
 	}
 
-	override fun forceDebug(message: String, vararg objects: Any) {
+	override fun forceDebug(message: String, vararg objects: Any?) {
 		logger.debug(generateFormat(message, *objects), *objects)
 	}
 
-	override fun forceInfo(message: String, vararg objects: Any) {
+	override fun forceInfo(message: String, vararg objects: Any?) {
 		logger.info(generateFormat(message, *objects), *objects)
 	}
 
-	override fun forceWarn(message: String, vararg objects: Any) {
+	override fun forceWarn(message: String, vararg objects: Any?) {
 		logger.warn(generateFormat(message, *objects), *objects)
 	}
 
-	override fun forceError(message: String, vararg objects: Any) {
+	override fun forceError(message: String, vararg objects: Any?) {
 		logger.error(generateFormat(message, *objects), *objects)
 	}
 
