af54292913cfc165b601cce9536422f3f6d9fa12
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-12-31 21:38:12 +0100

    style(state): Explicitly specify the return type of the Slice factories

--- state/src/commonMain/kotlin/opensavvy.state/slice/Factories.kt
+++ state/src/commonMain/kotlin/opensavvy.state/slice/Factories.kt
@@ -4,8 +4,12 @@ import arrow.core.left
 import arrow.core.right
 import opensavvy.state.Failure
 
-fun <T> successful(value: T) =
+fun <T> successful(value: T): Slice<T> =
 	value.right()
 
-fun failed(message: String, kind: Failure.Kind = Failure.Kind.Unknown, cause: RuntimeException? = null) =
+fun failed(
+	message: String,
+	kind: Failure.Kind = Failure.Kind.Unknown,
+	cause: RuntimeException? = null,
+): Slice<Nothing> =
 	Failure(kind, message, cause).left()
