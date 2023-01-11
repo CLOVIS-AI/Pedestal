cdbd0ac91f497afc0bf49870accdc5349fd57903
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-01-11 22:05:28 +0100

    feat(spine): Serialize IDs as a simple string instead of an object

--- spine/src/commonTest/kotlin/IdTest.kt
+++ spine/src/commonTest/kotlin/IdTest.kt
@@ -1,5 +1,8 @@
 package opensavvy.spine
 
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import opensavvy.spine.Route.Companion.div
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -11,5 +14,16 @@ class IdTest {
 		val id = Id(service = "v2", resource = Route / "int" / "35eb")
 
 		assertEquals("v2/int/35eb", id.toString())
+	}
+
+	@Test
+	fun serialize() {
+		val id = Id(service = "v2", resource = Route / "int" / "35eb")
+
+		val serialized = Json.encodeToString(id)
+		assertEquals("v2/int/35eb", id.toString())
+
+		val deserialized = Json.decodeFromString<Id>(serialized)
+		assertEquals(id, deserialized)
 	}
 }
