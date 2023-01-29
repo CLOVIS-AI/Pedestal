b0a2ffe868bf80c3ab7a2551e4ca71aec0057510
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-01-29 17:13:03 +0100

    feat(state): Added ProgressiveOutcome.map, Flow<ProgressiveOutcome>.mapSuccess

--- state/src/commonMain/kotlin/progressive/Conditional.kt
+++ state/src/commonMain/kotlin/progressive/Conditional.kt
@@ -1,7 +1,11 @@
 package opensavvy.state.progressive
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import opensavvy.state.Progression
 import opensavvy.state.progressive.ProgressiveOutcome.*
+
+//region Actions
 
 /**
  * Executes [block] if this outcome is [successful][Success].
@@ -37,3 +41,27 @@ inline fun <T> ProgressiveOutcome<T>.onLoading(block: (Progression.Loading) -> U
 	if (progression is Progression.Loading)
 		block(progression)
 }
+
+//endregion
+//region Combinations
+
+/**
+ * Replaces the value of this outcome if it is successful using [transform].
+ *
+ * If this outcome isn't successful, does nothing.
+ */
+inline fun <T, U> ProgressiveOutcome<T>.map(transform: (T) -> U) = when (this) {
+	is Empty -> this
+	is Failure -> this
+	is Success -> Success(transform(this.value), this.progress)
+}
+
+/**
+ * Replaces the successful values using [transform].
+ *
+ * Values that are not successful are unchanged.
+ */
+inline fun <T, U> Flow<ProgressiveOutcome<T>>.mapSuccess(crossinline transform: (T) -> U) = this
+	.map { it.map(transform) }
+
+//endregion
