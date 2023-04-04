1da3a80086e697d814235f0c9cbf9f7c6ac5c597
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-04-04 20:22:26 +0200

    refactor(state): Remove Flow.firstValue

--- state/src/commonMain/kotlin/State.kt
+++
@@ -1,9 +0,0 @@
-package opensavvy.state
-
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.firstOrNull
-import opensavvy.state.outcome.Outcome
-import opensavvy.state.outcome.failed
-
-suspend fun <T> Flow<Outcome<T>>.firstValue() = firstOrNull()
-	?: failed("The flow terminated before emitting a value", Failure.Kind.NotFound)
