9087f3538e953d8adf9abd52f7bba17dce96f4e6
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-04-29 19:00:17 +0200

    feat(state): Conversion function from Arrow to Outcome

--- state-arrow/src/commonMain/kotlin/Converter.kt
+++ state-arrow/src/commonMain/kotlin/Converter.kt
@@ -5,7 +5,10 @@ import arrow.core.left
 import arrow.core.right
 import opensavvy.state.failure.Failure
 import opensavvy.state.outcome.Outcome
+import opensavvy.state.outcome.failed
+import opensavvy.state.outcome.success
 import opensavvy.state.progressive.ProgressiveOutcome
+import opensavvy.state.progressive.withProgress
 
 /**
  * Converts an [Outcome] into a typed [Either].
@@ -27,3 +30,21 @@ fun <F : Failure, T> ProgressiveOutcome<F, T>.toEither() = when (this) {
     is ProgressiveOutcome.Failure -> failure.left()
     is ProgressiveOutcome.Incomplete -> null
 }
+
+/**
+ * Converts an [Either] into an [Outcome].
+ *
+ * The [out] DSL is also useful to work with [Outcome] in the Arrow world.
+ */
+fun <F : Failure, T> Either<F, T>.toOutcome() = when (this) {
+    is Either.Right -> value.success()
+    is Either.Left -> value.failed()
+}
+
+/**
+ * Converts an [Either] into a [ProgressiveOutcome].
+ *
+ * The [out] DSL is also useful to work with [Outcome] in the Arrow world.
+ */
+fun <F : Failure, T> Either<F, T>.toOutcome(progress: Progress = loading()) = toOutcome()
+    .withProgress(progress)
