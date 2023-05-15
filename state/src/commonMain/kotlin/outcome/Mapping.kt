6e6bd639e15f4f5748f511cfd6e9ca34772945dc
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-05-15 22:20:51 +0200

    feat(state): Add Outcome.mapFailure

--- state/src/commonMain/kotlin/outcome/Mapping.kt
+++ state/src/commonMain/kotlin/outcome/Mapping.kt
@@ -9,3 +9,13 @@ inline fun <F, T, U> Outcome<F, T>.map(transform: (T) -> U) = when (this) {
 	is Outcome.Failure -> this
 	is Outcome.Success -> Outcome.Success(transform(this.value))
 }
+
+/**
+ * Replaces the failure of this outcome if it is failed using [transformFailure].
+ *
+ * If this outcome isn't failed, does nothing.
+ */
+inline fun <F, T, G> Outcome<F, T>.mapFailure(transformFailure: (F) -> G) = when (this) {
+	is Outcome.Failure -> Outcome.Failure(transformFailure(this.failure))
+	is Outcome.Success -> this
+}
