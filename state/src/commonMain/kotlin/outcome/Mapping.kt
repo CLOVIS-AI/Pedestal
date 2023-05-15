9181fcd90161a7a13504b9bf6e8ca390b4b47819
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2023-05-15 22:22:55 +0200

    docs(state): Clarify the documentation of map variants

--- state/src/commonMain/kotlin/outcome/Mapping.kt
+++ state/src/commonMain/kotlin/outcome/Mapping.kt
@@ -1,9 +1,11 @@
 package opensavvy.state.outcome
 
 /**
- * Replaces the value of this outcome if it is successful using [transform].
+ * If this outcome is [successful][Outcome.Success], replaces its [value][Outcome.Success.value] using [transform].
  *
  * If this outcome isn't successful, does nothing.
+ *
+ * @see mapFailure Map the failure state instead of the success state.
  */
 inline fun <F, T, U> Outcome<F, T>.map(transform: (T) -> U) = when (this) {
 	is Outcome.Failure -> this
@@ -11,9 +13,11 @@ inline fun <F, T, U> Outcome<F, T>.map(transform: (T) -> U) = when (this) {
 }
 
 /**
- * Replaces the failure of this outcome if it is failed using [transformFailure].
+ * If this outcome is [failed][Outcome.Failure], replaces its [failure][Outcome.Failure.failure] using [transformFailure].
  *
  * If this outcome isn't failed, does nothing.
+ *
+ * @see map Map the success state instead of the failure state.
  */
 inline fun <F, T, G> Outcome<F, T>.mapFailure(transformFailure: (F) -> G) = when (this) {
 	is Outcome.Failure -> Outcome.Failure(transformFailure(this.failure))
--- state/src/commonMain/kotlin/progressive/Mapping.kt
+++ state/src/commonMain/kotlin/progressive/Mapping.kt
@@ -1,9 +1,11 @@
 package opensavvy.state.progressive
 
 /**
- * Replaces the value of this outcome if it is successful using [transform].
+ * If this outcome is [successful][ProgressiveOutcome.Success], replaces its [value][ProgressiveOutcome.Success.value] using [transform].
  *
  * If this outcome isn't successful, does nothing.
+ *
+ * @see mapFailure Map the failure state instead of the success state.
  */
 inline fun <F, T, U> ProgressiveOutcome<F, T>.map(transform: (T) -> U) = when (this) {
     is ProgressiveOutcome.Incomplete -> this
@@ -12,9 +14,11 @@ inline fun <F, T, U> ProgressiveOutcome<F, T>.map(transform: (T) -> U) = when (t
 }
 
 /**
- * Replaces the value of this outcome if it is failed using [transformFailure].
+ * If this outcome is [failed][ProgressiveOutcome.Failure], replaces its [failure][ProgressiveOutcome.Failure.failure] using [transformFailure].
  *
  * If this outcome isn't failed, does nothing.
+ *
+ * @see map Map the success state instead of the failure state.
  */
 inline fun <F, T, G> ProgressiveOutcome<F, T>.mapFailure(transformFailure: (F) -> G) = when (this) {
     is ProgressiveOutcome.Incomplete -> this
