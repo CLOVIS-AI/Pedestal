a0b38bfbdb9f8fd1c65d86baa6ba9ccefd4edd66
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2024-09-06 21:42:02 +0200

    feat(weak): Create the ExperimentalWeakApi

---
+++ weak/src/commonMain/kotlin/ExperimentalWeakApi.kt
@@ -0,0 +1,5 @@
+package opensavvy.pedestal.weak
+
+@RequiresOptIn("This API is waiting for feedback before stabilization. Please send us feedback at https://gitlab.com/opensavvy/groundwork/pedestal/-/issues/150", RequiresOptIn.Level.ERROR)
+@MustBeDocumented
+annotation class ExperimentalWeakApi
