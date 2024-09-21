691af7171a2f31c421f4b89688185e19168cf511
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2024-09-21 16:07:04 +0200

    test(state): Migrate ProgressiveOutcome tests to Prepared

--- state/src/commonTest/kotlin/progressive/AccessorsTest.kt
+++ state/src/commonTest/kotlin/progressive/AccessorsTest.kt
@@ -1,123 +1,90 @@
 package opensavvy.state.progressive
 
+import opensavvy.prepared.runner.kotest.PreparedSpec
 import opensavvy.progress.loading
 import opensavvy.state.ExperimentalProgressiveRaiseApi
 import opensavvy.state.outcome.Outcome
-import kotlin.test.Test
 import kotlin.test.assertEquals
 
+class AccessorsTest : PreparedSpec({
 
-class AccessorsTest {
+	@Suppress("LocalVariableName")
+	val Failed = "FAILED"
 
-    private object Failed
+	@OptIn(ExperimentalProgressiveRaiseApi::class)
+	suite("Access progress of unsuccessful values") {
+		test("Failure") {
+			val failure = ProgressiveOutcome.Failure(Unit, loading(0.13)) as ProgressiveOutcome.Unsuccessful<Unit>
+			assertEquals(
+				loading(0.13),
+				failure.progress
+			)
+		}
 
-    // region Unsuccessful
+		test("Incomplete") {
+			val failure = ProgressiveOutcome.Incomplete(loading(0.13)) as ProgressiveOutcome.Unsuccessful<Unit>
+			assertEquals(
+				loading(0.13),
+				failure.progress
+			)
+		}
+	}
 
-    @OptIn(ExperimentalProgressiveRaiseApi::class)
-    @Test
-    fun unsuccessfulProgressFailure() {
-        val failure = ProgressiveOutcome.Failure(Unit, loading(0.13)) as ProgressiveOutcome.Unsuccessful<Unit>
-        assertEquals(
-            loading(0.13),
-            failure.progress
-        )
-    }
+	suite("valueOrNull") {
+		test("Success") {
+			check(5.successfulWithProgress().valueOrNull == 5)
+		}
 
-    @OptIn(ExperimentalProgressiveRaiseApi::class)
-    @Test
-    fun unsuccessfulProgressIncomplete() {
-        val failure = ProgressiveOutcome.Incomplete(loading(0.13)) as ProgressiveOutcome.Unsuccessful<Unit>
-        assertEquals(
-            loading(0.13),
-            failure.progress
-        )
-    }
+		test("Failure") {
+			@Suppress("IMPLICIT_NOTHING_TYPE_ARGUMENT_IN_RETURN_POSITION", "SENSELESS_COMPARISON")
+			check(Failed.failedWithProgress().valueOrNull == null)
+		}
+	}
 
-    // endregion
-    // region Get or null
+	suite("failureOrNull") {
+		test("Success") {
+			@Suppress("IMPLICIT_NOTHING_TYPE_ARGUMENT_IN_RETURN_POSITION", "SENSELESS_COMPARISON")
+			check(5.successfulWithProgress().failureOrNull == null)
+		}
 
-    @Test
-    fun valueOnSuccess() {
-        assertEquals(
-            5,
-            5.successfulWithProgress().valueOrNull,
-        )
-    }
+		test("Failure") {
+			check(Failed.failedWithProgress().failureOrNull == Failed)
+		}
+	}
 
-    @Suppress("IMPLICIT_NOTHING_TYPE_ARGUMENT_IN_RETURN_POSITION") // it's the goal of the test!
-    @Test
-    fun valueOnFailure() {
-        assertEquals<Int?>(
-            null,
-            Failed.failedWithProgress().valueOrNull,
-        )
-    }
+	suite("Nothing variant") {
+		test("Success") {
+			check((5.successfulWithProgress() as ProgressiveOutcome<Nothing, Int>).value == 5)
+		}
 
-    @Suppress("IMPLICIT_NOTHING_TYPE_ARGUMENT_IN_RETURN_POSITION") // it's the goal of the test!
-    @Test
-    fun failureOnSuccess() {
-        assertEquals(
-            null,
-            5.successfulWithProgress().failureOrNull,
-        )
-    }
+		test("Failure") {
+			check((Failed.failedWithProgress() as ProgressiveOutcome<String, Nothing>).failure == Failed)
+		}
+	}
 
-    @Test
-    fun failureOnFailure() {
-        assertEquals(
-            Failed,
-            Failed.failedWithProgress().failureOrNull,
-        )
-    }
+	suite("Destructuring") {
+		test("Success") {
+			val value: ProgressiveOutcome<*, Int> = ProgressiveOutcome.Success(5, loading(0.23))
+			val (outcome, progress) = value
 
-    // endregion
-    // region Nothing variant
+			assertEquals(Outcome.Success(5), outcome)
+			assertEquals(loading(0.23), progress)
+		}
 
-    @Test
-    fun valueOnSuccessNothing() {
-        assertEquals(
-            5,
-            (5.successfulWithProgress() as ProgressiveOutcome<Nothing, Int>).value,
-        )
-    }
+		test("Failure") {
+			val value: ProgressiveOutcome<*, Int> = ProgressiveOutcome.Failure(Failed, loading(0.23))
+			val (outcome, progress) = value
 
-    @Test
-    fun failureOnFailureNothing() {
-        assertEquals(
-            Failed,
-            (Failed.failedWithProgress() as ProgressiveOutcome<Failed, Nothing>).failure,
-        )
-    }
+			assertEquals(Outcome.Failure(Failed), outcome)
+			assertEquals(loading(0.23), progress)
+		}
 
-    // endregion
-    // region Destructuration
+		test("Incomplete") {
+			val value: ProgressiveOutcome<*, Int> = ProgressiveOutcome.Incomplete(loading(0.23))
+			val (outcome, progress) = value
 
-    @Test
-    fun destructurationOnSuccess() {
-        val value: ProgressiveOutcome<*, Int> = ProgressiveOutcome.Success(5, loading(0.23))
-        val (outcome, progress) = value
-
-        assertEquals(Outcome.Success(5), outcome)
-        assertEquals(loading(0.23), progress)
-    }
-
-    @Test
-    fun destructurationOnFailure() {
-        val value: ProgressiveOutcome<*, Int> = ProgressiveOutcome.Failure(Failed, loading(0.23))
-        val (outcome, progress) = value
-
-        assertEquals(Outcome.Failure(Failed), outcome)
-        assertEquals(loading(0.23), progress)
-    }
-
-    @Test
-    fun destructurationOnIncomplete() {
-        val value: ProgressiveOutcome<*, Int> = ProgressiveOutcome.Incomplete(loading(0.23))
-        val (outcome, progress) = value
-
-        assertEquals(null, outcome)
-        assertEquals(loading(0.23), progress)
-    }
-
-    // endregion
-}
+			assertEquals(null, outcome)
+			assertEquals(loading(0.23), progress)
+		}
+	}
+})
