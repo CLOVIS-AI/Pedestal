0078dc2ea3cbb5b16cd5bb5974c04c3918f2e4e7
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2025-02-16 21:57:15 +0100

    docs(weak): Add copyright headers

--- weak/src/commonMain/kotlin/ExperimentalWeakApi.kt
+++ weak/src/commonMain/kotlin/ExperimentalWeakApi.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2024-2025, OpenSavvy and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package opensavvy.pedestal.weak
 
 @RequiresOptIn("This API is waiting for feedback before stabilization. Please send us feedback at https://gitlab.com/opensavvy/groundwork/pedestal/-/issues/150", RequiresOptIn.Level.ERROR)
