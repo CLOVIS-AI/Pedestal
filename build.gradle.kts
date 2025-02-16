da40f829bd3d04a3d20496218a785ff2a7b91ab7
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2025-02-16 22:07:44 +0100

    build(enumset): Create the EnumSet module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2025, OpenSavvy and contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
@@ -30,6 +46,7 @@ dependencies {
 	dokka(projects.state)
 	dokka(projects.stateCoroutines)
 	dokka(projects.weak)
+	dokka(projects.enumset)
 
 	kover(projects.backbone)
 	kover(projects.cache)
@@ -38,6 +55,7 @@ dependencies {
 	kover(projects.progressCoroutines)
 	kover(projects.state)
 	kover(projects.stateCoroutines)
+	kover(projects.enumset)
 }
 
 // region Check the users of the project didn't forget to rename the group
---
+++ enumset/README.md
@@ -0,0 +1,9 @@
+# Module EnumSet
+
+Efficient `BitSet` and `EnumSet` implementations for Kotlin Multiplatform.
+
+<a href="https://search.maven.org/search?q=g:%22dev.opensavvy.pedestal%22%20AND%20a:%22enumset%22"><img src="https://img.shields.io/maven-central/v/dev.opensavvy.pedestal/enumset.svg?label=Maven%20Central"></a>
+<a href="https://opensavvy.dev/open-source/stability.html"><img src="https://badgen.net/static/Stability/stable/purple"></a>
+<a href="https://javadoc.io/doc/dev.opensavvy.pedestal/enumset"><img src="https://badgen.net/static/Other%20versions/javadoc.io/blue"></a>
+
+# Package opensavvy.enumset
