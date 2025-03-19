d91417ac6b331334bba2be03bba0a6c13f8c77f7
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2025-03-19 21:23:07 +0100

    feat(enumset): Create the MutableBitSet32 class

--- enumset/src/commonTest/kotlin/datatypes/SetSuite.kt
+++ enumset/src/commonTest/kotlin/datatypes/SetSuite.kt
@@ -14,11 +14,17 @@
  * limitations under the License.
  */
 
+@file:OptIn(ExperimentalParameterizeApi::class)
+
 package opensavvy.enumset.datatypes
 
+import com.benwoodworth.parameterize.ExperimentalParameterizeApi
+import com.benwoodworth.parameterize.parameterOf
+import com.benwoodworth.parameterize.parameterize
 import io.kotest.assertions.throwables.shouldThrow
 import opensavvy.prepared.suite.Prepared
 import opensavvy.prepared.suite.SuiteDsl
+import opensavvy.prepared.suite.TestDsl
 import opensavvy.prepared.suite.prepared
 import opensavvy.prepared.suite.random.nextInt
 import opensavvy.prepared.suite.random.random
@@ -98,14 +104,9 @@ fun SuiteDsl.testSetValidity(
 	}
 
 	suite("Multiple elements") {
-		val targetSize by randomInt(0, maxSize - 1)
+		val targetSize by randomInt(1, maxSize - 1)
 		val values by prepared {
-			val size = targetSize()
-			buildSet {
-				while (this.size < size) {
-					add(random.nextInt(0, maxSize))
-				}
-			}
+			buildSetOfSize(targetSize(), maxSize)
 		}
 		val set by prepared {
 			create(values().toTypedArray())
@@ -243,3 +244,204 @@ fun SuiteDsl.testFullSetValidity(
 		check(maxSize + 2 !in set())
 	}
 }
+
+fun SuiteDsl.testMutableSetValidity(
+	name: String,
+	maxSize: Int,
+	create: (Array<Int>) -> MutableSet<Int>,
+) = suite("MutableSet $name") {
+	testSetValidity(name, maxSize, create)
+
+	suite("clear") {
+		val clearedSet by prepared {
+			val set = create(arrayOf(0, 12))
+			set.clear()
+			set
+		}
+
+		testEmptySetValidity(name, maxSize, clearedSet)
+	}
+
+	suite("add") {
+		test("Add an element to the empty set") {
+			val set = create(emptyArray())
+			check(set.add(5))
+			check(5 in set)
+			check(set.size == 1)
+		}
+
+		test("Add an element to a non-empty set") {
+			val set = create(arrayOf(0, 12))
+			check(set.add(5))
+			check(5 in set)
+			check(0 in set)
+			check(12 in set)
+			check(set.size == 3)
+		}
+
+		test("Add an element that is already present") {
+			val set = create(arrayOf(0, 12))
+			check(!set.add(12))
+			check(0 in set)
+			check(12 in set)
+			check(set.size == 2)
+		}
+	}
+
+	suite("addAll") {
+		parameterize {
+			val currentSize by parameterOf(0, 1, 2, 12, maxSize - 1)
+
+			val set by prepared {
+				create(buildSetOfSize(currentSize, maxSize).toTypedArray())
+			}
+
+			val addingSize by parameterOf(0, 1, 4)
+
+			val addingValues by prepared {
+				buildSetOfSize(addingSize, maxSize)
+			}
+
+			val addingSet by parameterOf(
+				prepared("set of any type") {
+					addingValues()
+				},
+				prepared("set of same type") {
+					create(addingValues().toTypedArray())
+				}
+			)
+
+			test("Adding $addingSize elements to a set of size $currentSize (${addingSet.name})") {
+				val set = set()
+
+				val willBeAdded = addingSet().filter { it !in set }
+
+				check(set.addAll(addingSet()) == willBeAdded.isNotEmpty())
+				check(set.size == currentSize + willBeAdded.size)
+			}
+		}
+	}
+
+	suite("remove") {
+		test("Remove an element from the empty set") {
+			val set = create(emptyArray())
+			check(!set.remove(5))
+			check(set.isEmpty())
+		}
+
+		test("Remove the only element in a set") {
+			val set = create(arrayOf(5))
+			check(set.remove(5))
+			check(set.isEmpty())
+		}
+
+		test("Remove an element that is not contained in the set") {
+			val set = create(arrayOf(4, 9))
+			check(!set.remove(5))
+			check(!set.isEmpty())
+			check(set.size == 2)
+		}
+
+		test("Remove an element in a set with multiple elements") {
+			val set = create(arrayOf(1, 9, 3))
+			check(set.remove(3))
+			check(!set.isEmpty())
+			check(set.size == 2)
+		}
+
+		for (i in listOf(-1, maxSize, maxSize + 1, Int.MAX_VALUE, Int.MIN_VALUE)) {
+			test("Remove an element that is out of range: $i") {
+				val set = create(arrayOf(7, 6))
+				check(!set.remove(i))
+				check(!set.isEmpty())
+				check(set.size == 2)
+			}
+		}
+	}
+
+	suite("removeAll") {
+		parameterize {
+			val currentSize by parameterOf(0, 1, 2, 12, maxSize - 1)
+
+			val set by prepared {
+				create(buildSetOfSize(currentSize, maxSize).toTypedArray())
+			}
+
+			val removingSize by parameterOf(0, 1, 4)
+
+			val removingValues by prepared {
+				buildSetOfSize(removingSize, maxSize)
+			}
+
+			val removingSet by parameterOf(
+				prepared("set of any type") {
+					removingValues()
+				},
+				prepared("set of same type") {
+					create(removingValues().toTypedArray())
+				}
+			)
+
+			test("Removing $removingSize elements from a set of size $currentSize (${removingSet.name})") {
+				val set = set()
+
+				val willBeRemoved = removingSet().filter { it in set }
+
+				check(set.removeAll(removingSet()) == willBeRemoved.isNotEmpty())
+				check(set.size == currentSize - willBeRemoved.size)
+			}
+		}
+	}
+
+	suite("Iterator") {
+		test("Remove an item while iterating") {
+			val set = create(arrayOf(5, 7, 9))
+			println("Iterating through set $set")
+			val iter = set.iterator()
+
+			check(iter.hasNext())
+			check(iter.next() == 5)
+
+			check(iter.hasNext())
+			iter.remove()
+
+			check(iter.hasNext())
+			check(iter.next() == 9)
+		}
+	}
+
+	suite("retainAll") {
+		test("Remove all elements") {
+			val set = create(arrayOf(5, 7, 9))
+
+			check(set.retainAll(emptySet()))
+			check(set.isEmpty())
+		}
+
+		test("Remove a single element") {
+			val set = create(arrayOf(5, 7, 9))
+
+			check(set.retainAll(setOf(1, 5, 9)))
+			check(!set.isEmpty())
+			check(set.size == 2)
+		}
+
+		test("Remove no elements") {
+			val set = create(arrayOf(5, 7, 9))
+
+			check(!set.retainAll(setOf(5, 7, 9)))
+			check(!set.isEmpty())
+			check(set.size == 3)
+		}
+	}
+}
+
+private suspend fun TestDsl.buildSetOfSize(
+	size: Int,
+	maxSize: Int,
+): Set<Int> =
+	buildSet {
+		while (this.size < size) {
+			add(random.nextInt(0, maxSize - 1))
+		}
+	}
